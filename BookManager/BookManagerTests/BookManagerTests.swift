a3334dd0ddbc09809356634be0ce2c32d86b8daa
Emiliano Magana <websmithmxl@gmail.com>
2025-10-18 18:49:48 -0700

    Corrected data storage

---
+++ BookManager/BookManagerTests/BookManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookManagerTests.swift
+//  BookManagerTests
+//
+//  Created by mbair on 10/18/25.
+//
+
+import Testing
+@testable import BookManager
+
+struct BookManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
